e4467e8aa53a987fcef625e8d34e131c26635c11
Jessie Monterroso <montj@fordav.com>
2019-04-27 19:27:02 -0500

    Small fix for SuggestionManager / RemoveSuggestion

--- src/commands/suggestions/RemoveSuggestion.ts
+++ src/commands/suggestions/RemoveSuggestion.ts
@@ -2,6 +2,8 @@ import {Command, CommandoClient, CommandoMessage} from "discord.js-commando";
 import {Message} from "discord.js";
 import {SuggestionsManager} from "../../utility/SuggestionsManager";
 
+const resultEnum = {LISTREMOVED: 8, SUGGESTIONREMOVED: 7, LISTEXISTS: 6, SUGGESTIONDOESNOTEXIST: 5, LISTADDED: 4, DERP: 3, SUGGESTIONADDED: 2, LISTDOESNOTEXIST: 1, SUGGESTIONEXISTS: 0};
+
 /**
  * A command for removeing a suggestion to the list of suggestions.
  */
@@ -15,7 +17,7 @@ export class RemoveSuggestion extends Command {
             name: "removesuggestion",
             group: "suggestions",
             memberName: "removesuggestion",
-            description: "Removes a suggestion and updates the file"
+            description: "Removes suggestion from the provided list. Commands should be formatted in LIST/SUGGESTION format. Including the /"
             });
     }
 
@@ -40,16 +42,21 @@ export class RemoveSuggestion extends Command {
         if (msg.guild == undefined)
             return msg.say("This command can only be executed in a guild.");
 
+        if (args.indexOf("/") == -1)
+            return msg.say("Command not formatted correctly. It should be in the LIST/SUGGESTION format. Don't forget the /");
+
         if (args != undefined && args.length > 0) {
             let result = SuggestionsManager.removesuggestion(args);
-            if (result)
-                return msg.say("Suggestion removed!");
-            else
+            if (result == resultEnum.LISTDOESNOTEXIST)
                 return msg.say("Can you not read? This was never suggested, fuck face.");
+            if (result == resultEnum.SUGGESTIONREMOVED)
+                return msg.say("Suggestion removed!");
+            if (result == resultEnum.SUGGESTIONDOESNOTEXIST)
+                return msg.say("This suggestion does not exist in the list provided.");
         }
-        else {
-            return msg.say("You somehow screwed up dumbass");
-        }
+        else
+            return msg.say("You get nothing. Good day, sir.");
+        return Promise.resolve(null);
     }
 }
 module.exports = RemoveSuggestion;
--- src/utility/SuggestionsManager.ts
+++ src/utility/SuggestionsManager.ts
@@ -7,7 +7,7 @@ import { SuggestionLists} from "./SuggestionLists";
  * Enum for potential results
  * @constant
  */
-const resultEnum = {LISTEXISTS: 6, SUGGESTIONDOESNOTEXIST: 5, LISTADDED: 4, DERP: 3, SUGGESTIONADDED: 2, LISTDOESNOTEXIST: 1, SUGGESTIONEXISTS: 0};
+const resultEnum = {LISTNOTEMPTY: 9, LISTREMOVED: 8, SUGGESTIONREMOVED: 7, LISTEXISTS: 6, SUGGESTIONDOESNOTEXIST: 5, LISTADDED: 4, DERP: 3, SUGGESTIONADDED: 2, LISTDOESNOTEXIST: 1, SUGGESTIONEXISTS: 0};
 
 /**
  * A static repository for holding suggestions for TrollBot
@@ -20,12 +20,13 @@ export abstract class SuggestionsManager {
 
     // private static _suggestions: Array<string> = new Array<string>();
     private static _suggestions: Collection<string, Array<string>> = new Collection<string, Array<string>>();
+    private static _suggestions2: Object = { string: Array<string>()};
 
     /**
      * Initializes the suggestions repository.
      */
     public static initialize() {
-       // SuggestionsManager.readsuggestionsFile();
+       SuggestionsManager.readsuggestionsFile();
     }
 
     /**
@@ -63,6 +64,7 @@ export abstract class SuggestionsManager {
         if ( SuggestionsManager._suggestions.has(tempList))
             return resultEnum.LISTEXISTS;
         SuggestionsManager._suggestions.set(tempList, new Array<string>());
+        this.updateFile();
         return resultEnum.LISTADDED;
     }
 
@@ -70,17 +72,67 @@ export abstract class SuggestionsManager {
      * Removes a suggestion from the list and updates the file.
      * @param suggestion The suggestion to remove.
      */
-    public static removesuggestion(suggestion: string): boolean | undefined {
+    public static removesuggestion(args: string): number | undefined {
+        let index = args.indexOf("/");
+        let tempList = args.substring(0, index).toLowerCase().trim();
+        let tempSuggestion = args.substring(index + 1).toLowerCase().trim();
 
-        return false;
+        if (!SuggestionsManager._suggestions.has(tempList))
+            return resultEnum.LISTDOESNOTEXIST;
+
+        let currentSuggestions = SuggestionsManager._suggestions.get(tempList);
+
+        if (currentSuggestions) {
+            for (let index = 0; index < currentSuggestions.length; index++) {
+                if (currentSuggestions[index] == tempSuggestion) {
+                    currentSuggestions.splice(index, 1);
+                    SuggestionsManager._suggestions.set(tempList, currentSuggestions);
+                    this.updateFile();
+                    return resultEnum.SUGGESTIONREMOVED;
+                }
+            }
+            return resultEnum.SUGGESTIONDOESNOTEXIST;
+        }
+        else {
+            return resultEnum.DERP; // This shouldn't happen..
+        }
+    }
+
+    /**
+     * Removes an empty list.
+     * @param args The list to remove.
+     */
+    public static removelist(args: string): number | undefined {
+        if (!SuggestionsManager._suggestions.has(args.toLowerCase().trim()))
+            return resultEnum.LISTDOESNOTEXIST;
+        let suggestionCheck = SuggestionsManager.getsuggestions(args);
+        if (suggestionCheck !== "List is empty. Add a suggestion!")
+            return resultEnum.LISTNOTEMPTY;
+        SuggestionsManager._suggestions.delete(args);
+        this.updateFile();
+        return resultEnum.LISTREMOVED;
     }
 
     /**
      * Updates the suggestion file. Used when user removes entry from the list.
      */
     public static updateFile() {
-        let toJSON = JSON.stringify(SuggestionsManager._suggestions);
+        let objToStringify = SuggestionsManager.collectionToObject();
+        let toJSON = JSON.stringify(objToStringify);
         fs.writeFileSync(ActBotConstants.suggestionsPath, toJSON);
+    }
+
+    /**
+     * Converts the collection of list/suggestions into an object for strigifying.
+     */
+    public static collectionToObject() {
+        let obj = Object.create(null);
+        for (let [k, v] of SuggestionsManager._suggestions) {
+            obj[k] = v;
+        }
+        let temp = JSON.stringify(obj);
+        // console.log(temp);
+        return obj;
     }
 
     /**
@@ -92,14 +144,18 @@ export abstract class SuggestionsManager {
             let suggestionsToReturn = SuggestionsManager._suggestions.get(templist);
             if (suggestionsToReturn!.toString() == "")
                 return "List is empty. Add a suggestion!";
-            console.log(suggestionsToReturn);
-            console.log(suggestionsToReturn!.toString());
-            let t1 = suggestionsToReturn!.toString();
-            let t2 = t1.replace(/,/g, "\n");
-            console.log(t2);
             return suggestionsToReturn!.toString().replace(/,/g, "\n");
         }
         return undefined;
+    }
+
+    /**
+     * Gets all lists.
+     */
+    public static getlists(): string | undefined {
+        let tempLists = SuggestionsManager._suggestions.keyArray();
+        // console.log(tempLists.toString()).replace(/,/g, "\n");
+        return tempLists.toString().replace(/,/g, "\n");
     }
 
     /**
@@ -110,9 +166,14 @@ export abstract class SuggestionsManager {
         SuggestionsManager._suggestions.clear();
         let tempString = fs.readFileSync(ActBotConstants.suggestionsPath).toString("utf-8");
         let tempParsed = JSON.parse(tempString) as Array<SuggestionLists>;
-        tempParsed.forEach(element => {
-            if (!SuggestionsManager._suggestions.has(element.ListName.toLowerCase()))
-                SuggestionsManager._suggestions.set(element.ListName.toLowerCase(), element.Suggestions);
-        });
+
+        for (let property in tempParsed) {
+            // console.log(property);
+            // console.log(tempParsed[property]);
+            if (!SuggestionsManager._suggestions.has(property.toLowerCase()))
+                SuggestionsManager._suggestions.set(property.toLowerCase(), tempParsed[property]);
+        }
+        let x = SuggestionsManager.collectionToObject();
+        console.log(x);
     }
 }
